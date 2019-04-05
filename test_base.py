d94664e7f908cb1ebbfb8aa2d29c4dcba330ff42
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-04-05 12:56:15 -0400

    Latest config/testing changes

--- test_base.py
+++ test_base.py
@@ -1,4 +1,4 @@
-from es_responder import app
+from es_responder import APP
 import unittest
 import time
 
@@ -6,71 +6,71 @@ NOW = int(time.time()) - 60
 
 class TestDiagnostics(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.APP = APP.test_client()
 
 # ******************************************************************************
 # * Doc/diagnostic endpoints                                                   *
 # ******************************************************************************
     def test_blank(self):
-        response = self.app.get('/')
+        response = self.APP.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_spec(self):
-        response = self.app.get('/spec')
+        response = self.APP.get('/spec')
         self.assertEqual(response.status_code, 200)
 
     def test_doc(self):
-        response = self.app.get('/doc')
+        response = self.APP.get('/doc')
         self.assertEqual(response.status_code, 200)
 
     def test_stats(self):
-        response = self.app.get('/stats')
+        response = self.APP.get('/stats')
         self.assertEqual(response.status_code, 200)
         self.assertGreater(response.json['stats']['requests'], 0)
 
 class TestContent(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.APP = APP.test_client()
 
 # ******************************************************************************
 # * General endpoints                                                          *
 # ******************************************************************************
     def test_query(self):
-        response = self.app.get('/query/dvid_combined_minute_summary')
+        response = self.APP.get('/query/dvid_combined_minute_summary')
         self.assertEqual(response.status_code, 200)
         self.assertGreaterEqual(response.json['result']['hits']['total'], 1)
-        response = self.app.get('/query/no_such_config')
+        response = self.APP.get('/query/no_such_config')
         self.assertEqual(response.status_code, 404)
 
     def test_metrics(self):
-        response = self.app.get('/metrics/*')
-        self.assertEqual(response.status_code, 301)
-        response = self.app.get('/metrics/*/1h')
+        response = self.APP.get('/metrics/*')
+        self.assertIn(response.status_code, [301, 308])
+        response = self.APP.get('/metrics/*/1h')
         self.assertEqual(response.status_code, 200)
         self.assertGreaterEqual(response.json['result']['count'], 100)
-        response = self.app.get('/metrics/ook/1h')
+        response = self.APP.get('/metrics/ook/1h')
         self.assertEqual(response.status_code, 404)
 
     def test_hits(self):
-        response = self.app.get('/hits/*')
+        response = self.APP.get('/hits/*')
         self.assertEqual(response.status_code, 400)
-        response = self.app.get('/hits/*?start=' + str(NOW-60) + '&end=' + str(NOW))
+        response = self.APP.get('/hits/*?start=' + str(NOW-60) + '&end=' + str(NOW))
         self.assertEqual(response.status_code, 200)
         self.assertGreaterEqual(response.json['result']['hits']['total'], 10)
-        response = self.app.get('/hits/*?method=get&start=' + str(NOW-60) + '&end=' + str(NOW))
+        response = self.APP.get('/hits/*?method=get&start=' + str(NOW-60) + '&end=' + str(NOW))
         self.assertEqual(response.status_code, 200)
         self.assertGreaterEqual(response.json['result']['hits']['total'], 1)
-        response = self.app.get('/hits/*?method=ook&start=' + str(NOW-60) + '&end=' + str(NOW))
+        response = self.APP.get('/hits/*?method=ook&start=' + str(NOW-60) + '&end=' + str(NOW))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['result']['hits']['total'], 0)
-        response = self.app.get('/hits/no_such_index?start=' + str(NOW-60) + '&end=' + str(NOW))
+        response = self.APP.get('/hits/no_such_index?start=' + str(NOW-60) + '&end=' + str(NOW))
         self.assertEqual(response.status_code, 404)
 
     def test_lasthits(self):
-        response = self.app.get('/lasthits/*/1')
+        response = self.APP.get('/lasthits/*/1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json['result']['hits']['hits']), 1)
-        response = self.app.get('/lasthits/no_such_index/1')
+        response = self.APP.get('/lasthits/no_such_index/1')
         self.assertEqual(response.status_code, 404)
 
 # ******************************************************************************
